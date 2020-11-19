e27edf4265bb9a777908f282ab3acb4b34f275b9
YYINGYG <1421190183@qq.com>
2020-11-19 16:01:27 +0800

    bug0

--- src/main/java/com/example/springhello3/service/UserService1.java
+++ src/main/java/com/example/springhello3/service/UserService1.java
@@ -3,6 +3,8 @@ package com.example.springhello3.service;
 public class UserService1 {
     public void add() {
         int x = 10;
-        int y = 10 / 0;
+        int y = 10 / x;
     }
+
+    public void fix() {}
 }
